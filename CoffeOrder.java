fe007f92af214c7b9156277cea3113ef3e10fcbf
Theyusuf41 <yusufakrglu@gmail.com>
2023-02-28 15:45:26 +0100

    Methods are created

---
+++ CoffeOrder.java
@@ -0,0 +1,10 @@
+public class CoffeOrder {
+    public static void main(String[] args) {
+        menuCall();
+        whichCoffe();
+        milkAdd();
+        sugarAdd();
+        sizeOfCoffee();
+
+    }
+}
